ea99343cdb2c3fcd0d76f313ee75dc6b24f26df2
Damien Wright <dww510@gmail.com>
2023-10-24 08:04:24 -0700

    lots of restructuring

--- astro.ts
+++ astro.ts
@@ -1,7 +1,7 @@
-export { default as Stack } from './src/component/Stack.astro';
+export { default as Stack } from './src/components/Stack.astro';
 
-export { default as ProductGrid } from './src/component/ProductGrid.astro';
-export { default as ProductCard } from './src/component/ProductCard.astro';
+export { default as ProductGrid } from './src/components/ProductGrid.astro';
+export { default as ProductCard } from './src/components/ProductCard.astro';
 
 export { default as StorePageLayout } from './src/StorePageLayout.astro';
 export { default as PageLayout } from './src/PageLayout.astro';
@@ -11,10 +11,10 @@ export { default as ProductPageWithSizePicker } from './src/ProductPageWithSizeP
 export { default as ProductWithColorPicker } from './src/ProductWithColorPicker.astro';
 export { default as ProductwithSizePicker } from './src/ProductWithSizePicker.astro';
 
-export { default as Hero } from './src/component/Hero.astro';
-export { default as Banner } from './src/component/Banner.astro';
-export { default as Text } from './src/component/Text.astro';
-export { default as Link } from './src/component/Link.astro';
-export { default as Button } from './src/component/Button.astro';
-export { default as Price } from './src/component/Price.astro';
+export { default as Hero } from './src/components/Hero.astro';
+export { default as Banner } from './src/components/Banner.astro';
+export { default as Text } from './src/components/Text.astro';
+export { default as Link } from './src/components/Link.astro';
+export { default as Button } from './src/components/Button.astro';
+export { default as Price } from './src/components/Price.astro';
 export { default as DefaultStoreIcon } from './src/DefaultStoreIcon.astro';
